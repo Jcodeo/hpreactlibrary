142f561ac1e8ac98b63b6abeb8c412bbf2dc42ca
Jcodeo <harsondev@gmail.com>
2021-12-12 19:50:22 +0300

    getting book list data from API

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import { useState } from 'react';
+import { useQuery } from 'react-query';
+
+export type CartItemType = {
+	cover : string;
+	title : string;
+	synopsis : string;
+	price : number;
+	amount : number;
+}
+const BOOKS_URL = "https://henri-potier.techx.fr/books";
+const getBooks = async (): Promise<CartItemType[]> => 
+	await (await fetch(BOOKS_URL)).json();
 
 const App = () => {
+  const { data, isLoading, error } = useQuery<CartItemType[]>(
+    'books', 
+    getBooks
+  );
+  console.log(data);
   return <div className="App">Start</div>;
 }
 
